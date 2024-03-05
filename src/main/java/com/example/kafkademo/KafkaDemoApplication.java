88bdc1f96fb08d3d6ec5f8c75f7ad22d9d79f947
Abhishek Patel <42441934+itsabhipatel@users.noreply.github.com>
2024-03-05 19:40:13 +0530

    Update KafkaDemoApplication.java

--- src/main/java/com/example/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafkademo/KafkaDemoApplication.java
@@ -8,7 +8,7 @@ public class KafkaDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaDemoApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello");
 	}
 
 }
