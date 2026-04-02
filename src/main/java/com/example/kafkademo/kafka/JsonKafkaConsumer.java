05fd761604f4446dab9d3304d401a4d5a1b55716
abhishek <abhi.patel0604@gmail.com>
2026-04-02 15:52:51 +0530

    Added files in featureA

--- src/main/java/com/example/kafkademo/kafka/JsonKafkaConsumer.java
+++ src/main/java/com/example/kafkademo/kafka/JsonKafkaConsumer.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class JsonKafkaConsumer {
 
     @KafkaListener(topics = {"kafka-demo","kafka-demo2"}, groupId = "myGroup")
-    public void consume(User message){
-        System.out.println("Consumed message: " + message);
+    public void consume(User m){
+        System.out.println("Consumed message: " + m);
     }
 }
